544f882a0320d89429844f3b862c6c4c2e481443
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2024-02-21 22:17:45 +0530

    Added fb-page-integration page

---
+++ client/app/fb-page-integration/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FBPageIntegrationCard } from '../components/FBPageIntegrationCard'
+
+const FBPageIntegration = () => {
+  return (
+    <div className = "bg-blue-900 min-h-screen min-w-screen flex items-center justify-center">
+        <FBPageIntegrationCard/>
+    </div>
+  )
+}
+
+export default FBPageIntegration;
